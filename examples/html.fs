08c044e9d1d78db55e2285265f325b4686d2b443
loscoala <info@fendersocke.de>
2022-09-11 17:05:47 +0200

    rewrite of html dict

--- examples/html.fs
+++ examples/html.fs
@@ -1,32 +1,31 @@
 #!goforth -file
 
 : document ." <!doctype html>" cr ;
+
 : tag ." <" exec ." >" ;
 : /tag ." </" exec ." >" ;
-: html [ ." html lang=\"de-DE\"" ] tag ;
-: /html [ ." html" ] /tag ;
-: head ." <head>" ;
-: /head ." </head>" ;
-: meta [ ." meta charset=\"utf-8\"" ] tag ;
-: title ." <title>" ;
-: /title ." </title>" ;
-: body ." <body>" ;
-: /body ." </body>" ;
-: p ." <p>" ;
-: /p ." </p>" ;
-: h1 ." <h1>" ;
-: /h1 ." </h1>" ;
+
+: enclose dup tag swap exec /tag ;
+
+: html [ ." html lang=\"de-DE\"" ] tag exec [ ." html" ] /tag ;
+: head [ ." head" ] enclose ;
+: body [ ." body" ] enclose ;
+
+: meta [ ." meta " exec ] tag ;
+: title [ ." title" ] enclose ;
+: p [ ." p" ] enclose ;
+: h1 [ ." h1" ] enclose ;
 
 : main
   document
-  html
-    head
-      meta
-      title ." Example Page" /title
-    /head
-    body
-      h1 ." Example Page h1" /h1
-      p ." Hello World!" /p
-    /body
-  /html
+  [
+    [
+      [ ." charset=\"utf-8\"" ] meta
+      [ ." Example Page" ] title
+    ] head
+    [
+      [ ." Example Page" ] h1
+      [ ." Hello World!" ] p
+    ] body
+  ] html
 ;
