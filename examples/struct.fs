ad8c335b1e6f8e15f198c6f25ae4f366683ac47a
loscoala <info@fendersocke.de>
2024-10-04 15:30:01 +0200

    struct example with parameters

--- examples/struct.fs
+++ examples/struct.fs
@@ -4,11 +4,11 @@
 struct foo 1 a 1 b 1 c
 
 \ This generates the following words
-\ foo:sizeof ( adr )
-\ foo:a ( adr )
-\ foo:b ( adr )
-\ foo:c ( adr )
-\ foo:[] ( index adr )
+\ foo:sizeof ( -- n )
+\ foo:a ( adr -- adr2 )
+\ foo:b ( adr -- adr2 )
+\ foo:c ( adr -- adr2 )
+\ foo:[] ( index adr -- adr2 )
 
 struct abc 25 a 12 b 10 c
 struct xyz 1 a 2 b 5 c
