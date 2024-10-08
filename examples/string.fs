43a6291fac653358f2aa401cbc62f613640392b8
loscoala <info@fendersocke.de>
2024-10-08 20:27:07 +0200

    string added

---
+++ examples/string.fs
@@ -0,0 +1,13 @@
+struct string 1 len 1 items
+
+: string:each ( block self -- )
+  { self block }
+  self string:len @ 0 ?do
+    self string:items i + @ block exec
+  loop
+;
+
+: string:print ( self -- )
+  [ emit ] swap string:each
+;
+
