e90f60a83fa9870f57528117f2afa998a22f5add
loscoala <info@fendersocke.de>
2024-10-10 15:55:32 +0200

    refactor

--- examples/dict.fs
+++ examples/dict.fs
@@ -1,11 +1,15 @@
 use list.fs
 
 ( *
-  * A dict is a list of pointer to kv structs
+  * A dict is a list of pointers to kv structs to key, value of strings
   * )
 
 struct dict extends list
 struct kv 1 key 1 value
+
+: kv:print ( self -- )
+  dup kv:key @ .s ."  : " kv:value @ .s
+;
 
 : dict:append ( k v self -- )
   1 kv:allot { kv self v k }
@@ -14,4 +18,7 @@ struct kv 1 key 1 value
   kv self list:append
 ;
 
-: dict:print ( self -- ) [ { kv } kv kv:key @ .s space colon space kv kv:value @ .s cr ] swap list:each ;
+\ prints dict[string][string]
+: dict:print ( self -- )
+  [ kv:print cr ] swap dict:each
+;
