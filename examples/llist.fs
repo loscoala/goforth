f3ce215434bdd0314fb6f98c7f5bbadffee81e43
loscoala <info@fendersocke.de>
2024-10-16 00:24:10 +0200

    linked list added

---
+++ examples/llist.fs
@@ -0,0 +1,54 @@
+: class list
+  1 len
+  1 head
+  1 tail
+;
+
+: class node
+  1 data
+  1 next
+;
+
+: node:print { self }
+  self node:data ? ."  -> " self node:next ?
+;
+
+: list:append { self }
+  1 node:allot { node }
+
+  node node:data !
+  0 node node:next !
+  
+  self list:len @ 0= if
+    node self list:head !
+    node self list:tail !
+  else
+    node self list:tail @ node:next !
+    node self list:tail !
+  then
+
+  self list:len @ 1+ self list:len !
+;
+
+: list:each { self block } 
+  self list:head @ { current }
+  begin
+    current 0<>
+  while
+    current block exec
+    current node:next @ to current
+  repeat
+;
+
+: list:print ( self -- )
+  [ node:print cr ] swap list:each
+;
+
+: list:test
+  1 list:allot drop
+  98 0 list:append
+  97 0 list:append
+  96 0 list:append
+  0 list:print
+;
+
