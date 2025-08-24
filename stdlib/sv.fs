99bc0ba7766a84c3682f8f080b67228fe52d18ea
loscoala <info@fendersocke.de>
2025-08-24 21:51:14 +0200

    use inline sv:_toS properly

--- stdlib/sv.fs
+++ stdlib/sv.fs
@@ -31,28 +31,27 @@
   drop
 ;
 
-: inline sv:_toS { _self }
-  _self sv:data @ _self sv:len @ 1- +
-  _self sv:data @
-  { _base _ptr }
+: inline sv:_toS
+  self sv:data @ self sv:len @ 1- +
+  self sv:data @
+  { base ptr }
   begin
-    _ptr _base >=
+    ptr base >=
   while
-    _ptr @
-    _ptr 1- to _ptr
+    ptr @
+    ptr 1- to ptr
   repeat
-  _self sv:len @
-  done
+  self sv:len @
   done
 ;
 
 : sv:toS { self }
   0
-  self sv:_toS
+  sv:_toS
 ;
 
 : sv:append { self other }
-  other sv:toS drop self sv:_toS drop
+  other sv:toS drop sv:_toS drop
   other sv:len @
   self sv:len @
   +
