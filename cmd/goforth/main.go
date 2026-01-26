5f65044f95eb34546f515de9d41fa113bb1447bd
loscoala <info@fendersocke.de>
2026-01-26 12:05:10 +0100

    compiler flags added and description improved

--- cmd/goforth/main.go
+++ cmd/goforth/main.go
@@ -10,13 +10,16 @@ var (
 	fname   string
 	script  string
 	compile bool
+	outfile string
 )
 
 func initFlags() {
-	flag.StringVar(&fname, "file", "", "compile file and execute")
+	flag.StringVar(&fname, "file", "", "Program passed in as a file")
 	flag.BoolVar(&goforth.Colored, "color", true, "Use colors")
-	flag.StringVar(&script, "script", "", "program passed in as string")
-	flag.BoolVar(&compile, "compile", false, "compile and run")
+	flag.StringVar(&script, "script", "", "Program passed in as string")
+	flag.BoolVar(&compile, "compile", false, "Compile to C")
+	flag.StringVar(&outfile, "o", "main", "The name of the generated binary file (-compile flag is required)")
+	flag.BoolVar(&goforth.CAutoExecute, "run", goforth.CAutoExecute, "Automatically execute the binary after compiling")
 
 	flag.Parse()
 }
@@ -39,6 +42,11 @@ func main() {
 	// load the core words
 	if err := fc.ParseFile("core"); err != nil {
 		goforth.PrintError(err)
+	}
+
+	if len(outfile) > 0 {
+		goforth.CBinaryName = outfile
+		goforth.CCodeName = outfile + ".c"
 	}
 
 	if len(script) > 0 {
